a3fd7b4d49472f87e99862e2f772dde61fca7896
adham-66 <adhamhewalla66@gmail.com>
2023-09-13 17:39:09 +0300

    add serializers, views & urls

--- django/blog_api/urls.py
+++ django/blog_api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from .views import PostList, PostDetail
 
 app_name = 'blog_api'
 
 urlpatterns = [
-
+    path('posts/<int:pk>/', PostDetail.as_view(), name='detailcreate'),
+    path('posts/', PostList.as_view(), name='listcreate'),
 ]
